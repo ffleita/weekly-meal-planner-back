221d6885b17f64818425fc5972203cc990a7bbc4
ffleita <facundoezequielfleitas96@gmail.com>
2025-06-11 11:35:51 -0300

    Agrego exceptions custom

---
+++ src/main/java/ffleitas/back/exceptions/DependenciasActivasException.java
@@ -0,0 +1,7 @@
+package ffleitas.back.exceptions;
+
+public class DependenciasActivasException extends RuntimeException {
+  public DependenciasActivasException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/ffleitas/back/exceptions/ElementoInexistenteException.java
@@ -0,0 +1,7 @@
+package ffleitas.back.exceptions;
+
+public class ElementoInexistenteException extends RuntimeException {
+    public ElementoInexistenteException(String message) {
+        super(message);
+    }
+}
