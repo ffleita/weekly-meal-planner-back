b1fe7d3236833dee17f7d2cc42a50224e28df021
ffleita <facundoezequielfleitas96@gmail.com>
2025-06-29 16:46:37 -0300

    Agrego repositorio para plan semanal

---
+++ src/main/java/ffleitas/back/domain/repositories/PlanRepository.java
@@ -0,0 +1,13 @@
+package ffleitas.back.domain.repositories;
+
+import ffleitas.back.domain.entities.PlanSemanal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PlanRepository extends JpaRepository<PlanSemanal, Long> {
+
+    @Query("SELECT ps FROM PlanSemanal ps WHERE ps.borradoLogico = false")
+    List<PlanSemanal> listarPlanesSemanalesNoBorradoLogico();
+}
