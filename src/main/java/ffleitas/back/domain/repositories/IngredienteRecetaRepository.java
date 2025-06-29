809156b05517ccebbde6803483bf0a4554ed3d03
ffleita <facundoezequielfleitas96@gmail.com>
2025-06-29 15:46:10 -0300

    Ajusto query para no retornar ingredientesReceta con ingredientes invalidos

--- src/main/java/ffleitas/back/domain/repositories/IngredienteRecetaRepository.java
+++ src/main/java/ffleitas/back/domain/repositories/IngredienteRecetaRepository.java
@@ -11,6 +11,8 @@ import java.util.List;
 public interface IngredienteRecetaRepository extends JpaRepository<IngredienteReceta, Long>
 {
 
-	@Query("SELECT ir FROM IngredienteReceta ir WHERE ir.receta = :receta")
+	@Query("SELECT ir FROM IngredienteReceta ir" +
+			" INNER JOIN Ingrediente i on i = ir.ingrediente" +
+			" WHERE ir.receta = :receta and i.borradoLogico = false")
 	List<IngredienteReceta> findIngredienteRecetaByReceta(Receta receta);
 }
